598d7febfa2bb7278e31ab7c77aac5753a4eb248
kk <+>
2023-03-14 12:54:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(argument = "roller-skate"){
+    return `This Saturday, I want to ${argument}!`;
+}
+saturdayFun();
+
+let mondayWork = function(will="go to the office"){
+ return `This Monday, I will ${will}.`;
+}
+mondayWork();
+
+function wrapAdjective (sample="*") {
+  return function(stringOne="special"){
+    return `You are ${sample}${stringOne}${sample}!`;
+  }
+}
